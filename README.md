2c232e8c5b11934224b8c1a0b7665c61d60a3921
yuxwang1102 <275489887@qq.com>
2019-03-18 15:00:58 +0800

    ptb

---
+++ README.md
@@ -0,0 +1,4 @@
+
+```bash      
+python main.py --cuda --emsize 1500 --nhid 1500 --dropout 0.65 --epochs 40 --tied 
+```
---
+++ run.sh
@@ -0,0 +1,3 @@
+#! /bin/bash  
+#!/usr/bin/env python
+python main.py --cuda --embedding_size 1500 --num_hid_unit 1500 --dropout 0.65 --epochs 1 --tied
